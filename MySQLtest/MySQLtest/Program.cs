dbd3f03f6aef7b96c8a6004aad36ccad134d23e5
koljoka <kalle.koljonen@edu.savonia.fi>
2024-02-04 14:36:01 +0200

    MySQLtest lisäys

---
+++ MySQLtest/MySQLtest/Program.cs
@@ -0,0 +1,50 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace MySQLtest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string server = "localhost";
+            string database = "hovi";
+            string username = "root";
+            string password = "Salasana-1212";
+           // int port = 3306;
+            string constring = "SERVER=" + server + ";" + "DATABASE=" + database + ";" +
+            "UID=" + username + ";" + "PASSWORD=" + password + ";";
+
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(constring))
+                {
+                    conn.Open();
+                    Console.WriteLine("Yhteys tietokantaan onnistui.");
+                    string insertQuery = "INSERT INTO henkilo (htun, enimi) VALUES (@htun, @enimi);";
+
+                    using (MySqlCommand cmd = new MySqlCommand(insertQuery, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@htun", "0210");
+                        cmd.Parameters.AddWithValue("@enimi", "Uniikki");
+
+                        int result = cmd.ExecuteNonQuery();
+                        if (result > 0)
+                        {
+                            Console.WriteLine("Henkilön nimi lisätty onnistuneesti.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Henkilön nimen lisääminen epäonnistui.");
+                        }
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Virhe yhdistettäessä tietokantaan: " + ex.Message);
+            }
+        }
+    }
+    
+}
