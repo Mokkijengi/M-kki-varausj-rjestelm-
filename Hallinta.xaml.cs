4cba214791c61f8a55100023007d8716a51d7739
ainomat <matilainen.aino1@gmail.com>
2024-02-04 14:42:29 +0200

    Muutos

--- Hallinta.xaml.cs
+++ Hallinta.xaml.cs
@@ -1,9 +1,23 @@
+using System.Collections.ObjectModel;
+
 namespace booking_VillageNewbies;
 
 public partial class Hallinta : ContentPage
 {
-	public Hallinta()
+    public Hallinta()
 	{
 		InitializeComponent();
-	}
+
+
+
+        // Additional configurations for nameEntry if needed
+        nameEntry.TextChanged += NameEntry_TextChanged;
+        // Other properties can be set here as well
+    }
+    private void NameEntry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        // Handle the text changed event
+        // Example: Console.WriteLine("Text is now: " + e.NewTextValue);
+    }
+
 }
