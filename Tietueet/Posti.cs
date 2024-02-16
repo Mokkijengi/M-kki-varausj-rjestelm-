f78f842787d6bca1e8de6907ca52679672568abd
koljoka <kalle.koljonen@edu.savonia.fi>
2024-02-16 15:25:51 +0200

    Palautettiin tietueet kansio takaisin

---
+++ Tietueet/Posti.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Posti//Jokaisessa classissa parametrit suoraan ER-kaaviosta. Lisäksi osaan alustettu
+                  //listoija joita tullaan mahdollisesti tarvitsemaan.
+{
+    public int Postinro { get; set; }
+    public string Toimipaikka { get; set; }
+
+    public Posti()
+    {
+    }
+    public Posti(int postinro, string toimipaikka)
+    {
+        Postinro = postinro;
+        Toimipaikka = toimipaikka;
+    }
+}
---
+++ Tietueet/Varaus.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Varaus //Jokaisessa classissa parametrit suoraan ER-kaaviosta. Lisäksi osaan alustettu
+                    //listoija joita tullaan mahdollisesti tarvitsemaan.
+{
+    public int VarausId { get; set; }
+    public int AsiakasId { get; set; }
+    public int MokkiId { get; set; }
+    public DateTime VarattuPvm { get; set; }
+    public DateTime VahvistusPvm { get; set; }
+    public DateTime VarattuAlkupvm { get; set; }
+    public DateTime VarattuLoppupvm { get; set; }
+
+    // Navigointia varten alla olevat kolme viittausta, selvitellään onko tarpeen
+    public Asiakas Asiakas { get; set; }
+    public Mokki Mokki { get; set; }
+    public List<VarauksenPalvelut> VarauksenPalvelut { get; set; }
+
+
+    public Varaus()
+    {
+        VarauksenPalvelut = new List<VarauksenPalvelut>();
+    }
+
+    public Varaus(int varausId, int asiakasId, int mokkiId, DateTime varattuPvm,
+                  DateTime vahvistusPvm, DateTime varattuAlkupvm, DateTime varattuLoppupvm)
+    {
+        VarausId = varausId;
+        AsiakasId = asiakasId;
+        MokkiId = mokkiId;
+        VarattuPvm = varattuPvm;
+        VahvistusPvm = vahvistusPvm;
+        VarattuAlkupvm = varattuAlkupvm;
+        VarattuLoppupvm = varattuLoppupvm;
+        VarauksenPalvelut = new List<VarauksenPalvelut>();
+    }
+}
